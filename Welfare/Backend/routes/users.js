18f60c0b61d7be0c2d67d71e8741abf7dd8542a4
_B³nji <151368851+Viking-jnr@users.noreply.github.com>
2025-07-17 10:52:18 +0000

    Chandeg my picture storage location to Cloudinary Cloud Storage

--- Welfare/Backend/routes/users.js
+++ Welfare/Backend/routes/users.js
@@ -2,16 +2,31 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "uploads",
+        allowed_formats: ['jpg', 'png']
+    },
+});
+
+/*const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
-});
+});*/
 const upload = multer({ storage });
 
 router.post("/", upload.single("profile"), async (req, res) => {
   const { fullName, IDNo, PhoneNo, location, fieldOfficer } = req.body;
-  const profilePicURL = req.file ? req.file.path : null;
+  const profilePicURL = req.file?.path || null;
   const newDependent = JSON.parse(req.body.dependents || "[]");
 
   try {
